f0a2a49a7e685b208e0af4b12f0498193f85ba97
Kaspar Schiess <kaspar.schiess@geo.uzh.ch>
2011-10-07 17:33:24 +0200

    . #32

---
+++ go/gotour/slices.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+  p := []int{1,2,3,4,5,6}
+  
+  fmt.Println("p ==", p)
+  fmt.Println("p[1:4] ==", p[1:4])
+
+  // missing low index implies 0
+  fmt.Println("p[:3] ==", p[:3])
+
+  // missing high index implies len(s)
+  fmt.Println("p[4:] ==", p[4:])
+  
+  for i:=0; i<len(p)+1; i++ {
+    fmt.Println(p[i])
+  } 
+}
